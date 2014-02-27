fab23dd8d0b84f0d76496fb92baa322a31b8b4ec
Slemgrim <slemgrim@gmail.com>
2014-02-27 21:59:52 +0100

    moved elements into body

--- web/examples/01-minefield/index.dart
+++ web/examples/01-minefield/index.dart
@@ -6,22 +6,21 @@ void main(){
   Element example = querySelector('#container');
   Element reset = querySelector('.reset');
   Element gameMessage = querySelector('.game-message');
-  
+
   Minefield minefield = new Minefield(example);
-  
+
   reset.onClick.listen((_){
     gameMessage.classes.remove('show');
     minefield.setup();
   });
-  
+
   minefield.onWin.listen((_){
     gameMessage.classes.add('show');
     gameMessage.innerHtml = 'Win!';
   });
-  
+
   minefield.onGameOver.listen((_){
     gameMessage.classes.add('show');
     gameMessage.innerHtml = 'Boom!';
   });
-
 }
